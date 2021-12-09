2d0b59706639d395db9e9bb6ded87cd6d8edc20c
hoyipyik <hoyipyik@protonmail.com>
2021-12-09 18:56:13 +0800

    1st frontend finished

--- client/src/container/LogPage/LogPage.js
+++ client/src/container/LogPage/LogPage.js
@@ -12,12 +12,12 @@ const LogPage = (props) => {
         <div className='logPage'>
             <div id='logpage'>
               <div className='container'>
-              {props.loggedEmail!==""?
+              {props.loggedEmail===""?
               <div>{ props.signed ?
               
                 <Login logHandler={props.logHandler}/>:
                 <SignUp signedHandler={props.signedHandler}/>
-              }</div>:<p>You have logged as {props.loggedEmail}</p>}
+              }</div>:<p id="log-info">You have logged as {props.loggedEmail}</p>}
                 <div id='log-switch'>
                   <span className='select' name="true" onClick={()=>props.signedHandler(true)}>Login</span>
                   <span id='gap'>{" / "}</span>
