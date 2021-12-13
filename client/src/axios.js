8a0f69d8f2933d5e6887a6249f5c213c55e98395
hoyipyik <vancouver.jerry.h007@gmail.com>
2021-12-13 23:05:07 +0800

    update new url

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios"
 const instance = axios.create({
     // baseURL: "https://wa2-react-default-rtdb.asia-southeast1.firebasedatabase.app/"
     // baseURL: "http://123.56.107.143:4000"
-    baseURL: 'http://localhost:4000'
+    // baseURL: 'http://localhost:4000'
+    baseURL: 'http://118.31.55.244:4000'
 })
 
 export default instance
