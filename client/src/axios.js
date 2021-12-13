07dc52912b457654a9e26e36f7992a297476dd9b
hoyipyik <hoyipyik@protonmail.com>
2021-12-13 16:58:32 +0800

    local api

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 const instance = axios.create({
     // baseURL: "https://wa2-react-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    baseURL: "http://123.56.107.143:4000"
+    // baseURL: "http://123.56.107.143:4000"
+    baseURL: 'http://localhost:4000'
 })
 
 export default instance
